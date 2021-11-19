81af1a6356171172ef0c6bfa6864b5a03dbce45f
lucy.reed <lucy.reed@bt.com>
2021-11-19 12:47:37 +0000

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Comitt a change!!!'));
+app.get('/', (req, res) => res.send('Making a new change...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
